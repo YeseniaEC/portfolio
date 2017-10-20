be95a95b74daee048da54455373b53234ef8f57f
Yesenia <eliyessie@gmail.com>
2017-10-19 17:18:58 -0700

    added pictures, some hoveer effects on three of the divs. need to add more styling

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-function hamburgerOpen() {
-
-  document.getElementById("main").style.marginLeft = "25%";
-
-  document.getElementById("sidebarWrapper").style.width = "25%";
-
-  document.getElementById("sidebarWrapper").style.display= "block";
-
-  document.getElementById("openNav").style.display = 'none';
-
-}
-
-function hamburgerClose() {
-  document.getElementById("main").style.width = "0%";
-
-  document.getElementById("sidebarWrapper").style.display= "none";
-
-  document.getElementById("openNav").style.display = "inline-block";
-
-}
+// function hamburgerOpen() {
+//
+//   document.getElementById("main").style.marginLeft = "25%";
+//
+//   document.getElementById("sidebarWrapper").style.width = "25%";
+//
+//   document.getElementById("sidebarWrapper").style.display= "block";
+//
+//   document.getElementById("openNav").style.display = 'none';
+//
+// }
+//
+// function hamburgerClose() {
+//   document.getElementById("main").style.width = "0%";
+//
+//   document.getElementById("sidebarWrapper").style.display= "none";
+//
+//   document.getElementById("openNav").style.display = "inline-block";
+//
+// }
